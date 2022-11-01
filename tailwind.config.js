0b506a65be7136d2b09fcffab76dfcfbd99dd730
mojahige <mojahige@gmail.com>
2022-11-01 23:12:25 +0900

    Update CSS sample
    Use Tailwind CSS and CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./storybook/**/*.{js,ts,css}",
+    "./stories/**/*.{js,ts,jsx,tsx,mdx,css,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
